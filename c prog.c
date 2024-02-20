053794dab5fc44d963c9381f7b6c2dd4f21f0e81
Karan Singh <156638995+KaranSingh0790@users.noreply.github.com>
2024-02-20 11:43:31 +0530

    Create c prog.c

---
+++ c prog.c
@@ -0,0 +1,14 @@
+# c-programming
+#include <stdio.h>
+int main(){
+int a,b;
+printf("enter a :");
+scanf("%d", &a);
+
+printf("enter b :");
+scanf("%d", &b);
+
+int sum= a+b;
+printf("sum is: %d", sum);
+return 0;
+}
